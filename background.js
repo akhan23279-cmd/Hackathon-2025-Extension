0f500a7c63da8e8434c6954b550524eafc2afd1a
rancescolon <109361303+rancescolon@users.noreply.github.com>
2025-11-08 15:20:14 -0500

    expain
    added expanations to files

---
+++ background.js
@@ -0,0 +1,3 @@
+// the backend logic of the extension(works when even closed) and Handles browser events
+// the "server" of the extension
+
---
+++ content.js
@@ -0,0 +1,2 @@
+//Code for if the site needs to scrape site and do anything with it.
+// will have to message the background script API or popup to do anything with the data.
