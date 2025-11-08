a887288e0e8c678ca73455ad26d087a8ed99d5ce
rancescolon <109361303+rancescolon@users.noreply.github.com>
2025-11-08 18:30:07 -0500

    fix

--- content/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-export function saveOptions(key, value) {
-    chrome.storage.local.set({ [key]: value });
-}
-
-export async function getActiveTab() {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    return tab;
-}
